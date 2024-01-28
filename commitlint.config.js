47c218f2406e3412968424f4b8a2c3b378d14c9b
marxgeek <mgangbala610@gmail.com>
2024-01-28 18:41:24 +0100

    feat(site): [#1] initializing the project, the biome format and commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  plugins: [
+    {
+      rules: {
+        "check-issue-number": ({ subject }) => {
+          if (!subject) {
+            return [false, "Your subject is empty or there is a space missing before."];
+          }
+          return [true];
+        },
+      },
+    },
+  ],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "style",
+        "test",
+        "revert",
+        "chore",
+      ],
+    ],
+    "scope-enum": [2, "always", ["bo", "server", "siteapp", "site"]],
+    "check-issue-number": [2, "always"],
+  },
+};
