c3e79389d9f52bdf8faf88a71ac8c3626ea82065
MarxGeek-lab <mgangbala610@gmail.com>
2024-04-05 00:59:34 +0100

    feat: mise à jour

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ import haricotb from "@/assets/images/haricot2.webp";
 import mais from "../assets/images/maiis.jpg";
 import manioc from "../assets/images/manioc2.webp";
 import anana from "../assets/images/ananas.webp";
-import courge from "../assets/images/courge.webp";
+import courge from "../assets/images/courge.jpg";
 import potirond from "../assets/images/potirond.webp";
 import noix from "../assets/images/noix.webp";
 import info_icon from "../assets/images/information-6258.svg";
